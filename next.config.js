3c1c63d3fca945c06c4e42c37631e72618c60b93
gktjd1904 <80248494+gktjd1904@users.noreply.github.com>
2023-03-28 00:47:22 +0900

    feat : 배포오류수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/portfolio",
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
